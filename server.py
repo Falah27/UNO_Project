598fe18363ef90c739d0c8f58597339a2dd8a7b3
Fajrul Fallah <fallahfajrul21@gmail.com>
2026-07-12 20:30:21 +0700

    Gajadi deploy

--- server.py
+++ server.py
@@ -412,18 +412,19 @@ class ThreadingServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 
 def main():
-    # port = 8765
-    # if len(sys.argv) > 1:
-    #     try:
-    #         port = int(sys.argv[1])
-    #     except ValueError:
-    #         pass
-    port = int(os.environ.get("PORT", 8765))
+    port = 8765
     if len(sys.argv) > 1:
         try:
             port = int(sys.argv[1])
         except ValueError:
             pass
+
+    # port = int(os.environ.get("PORT", 8765))
+    # if len(sys.argv) > 1:
+    #     try:
+    #         port = int(sys.argv[1])
+    #     except ValueError:
+    #         pass
 
     threading.Thread(target=uno_timeout_watcher, daemon=True).start()
     threading.Thread(target=heartbeat_watcher, daemon=True).start()
